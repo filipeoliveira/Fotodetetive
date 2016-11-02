b6c78771a9da473bcb2ba868a145e503c30602e9
PedroVitor-PC\Pedro Vitor <pedrovitor.marques@gmail.com>
2016-11-02 13:12:35 -0200

    Player(camera) se movimenta

---
+++ Assets/Scripts/Player/Move.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+	
+	public float speed;
+
+	void Update () {
+		Moving ();
+	}
+
+	void Moving(){
+		transform.position += new Vector3 (Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"),0)*speed;
+	}
+}
