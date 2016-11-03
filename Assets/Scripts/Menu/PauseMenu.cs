7647f37c3a82de53b31d4c92cf7063554a9083de
PedroVitor-PC\Pedro Vitor <pedrovitor.marques@gmail.com>
2016-11-03 18:28:27 -0200

    Add tela de pause em Alpha (Muito feia e simples ainda)

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+	bool paused = false;
+
+	void Update()
+	{
+		if(Input.GetKeyDown("w"))
+			paused = togglePause();
+	}
+
+	void OnGUI()
+	{
+		if(paused)
+		{
+			GUILayout.Label ("Jogo Pausado");
+
+			if(GUILayout.Button("Voltar ao Jogo"))
+				paused = togglePause();
+		}
+	}
+
+	bool togglePause()
+	{
+		if(Time.timeScale == 0f)
+		{
+			Time.timeScale = 1f;
+			return(false);
+		}
+		else
+		{
+			Time.timeScale = 0f;
+			return(true);    
+		}
+	}
+}
