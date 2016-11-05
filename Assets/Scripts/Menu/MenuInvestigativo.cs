af1d0bdeaaf9dc5448102ef440fde4ad8ffcd85d
PedroVitor-PC\Pedro Vitor <pedrovitor.marques@gmail.com>
2016-11-05 13:04:13 -0200

    Ajustes

--- Assets/Scripts/Menu/MenuInvestigativo.cs
+++ Assets/Scripts/Menu/MenuInvestigativo.cs
@@ -6,5 +6,11 @@ public class MenuInvestigativo : MonoBehaviour {
 	public void MenuInvestigativoOnOff(){
 		transform.FindChild ("telaNormal").gameObject.SetActive (!transform.FindChild ("telaNormal").gameObject.activeSelf);
 		transform.FindChild ("menuInvestigativo").gameObject.SetActive (!transform.FindChild ("menuInvestigativo").gameObject.activeSelf);
+		ResetaRelacao ();
+	}
+
+	public void ResetaRelacao(){
+		transform.gameObject.GetComponentInChildren<RelateObjects> ().vetorSelecionados [0] = null;
+		transform.gameObject.GetComponentInChildren<RelateObjects> ().vetorSelecionados [1] = null;
 	}
 }
